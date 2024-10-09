99d3569e8854b669babf4db5ea4c579509b9b172
stropee <stropee@ampus-eni.fr>
2024-10-09 16:43:05 +0200

    Module 4 - TP 4

--- src/app/components/carbon-footprint/carbon-footprint.component.ts
+++ src/app/components/carbon-footprint/carbon-footprint.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {CarbonFootprintFormComponent} from "../carbon-footprint-form/carbon-footprint-form.component";
 import {CarbonFootprintResultComponent} from "../carbon-footprint-result/carbon-footprint-result.component";
 import {DecimalPipe, NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
+import {CarbonFootprintComputeService} from "../../services/carbon-footprint-compute.service";
 
 @Component({
   selector: 'app-carbon-footprint',
@@ -15,6 +16,7 @@ import {DecimalPipe, NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
     NgClass,
     NgForOf
   ],
+  providers : [CarbonFootprintComputeService],
   templateUrl: './carbon-footprint.component.html',
   styleUrl: './carbon-footprint.component.css'
 })
@@ -22,16 +24,11 @@ export class CarbonFootprintComponent {
 
   public distance: number = 0
   public consumptionFor100Km: number = 0
-  public travels: Array<any>
+  public co2 : number = 0
+  public travels : any[]
 
-  constructor() {
-    this.travels = [
-      {distance: 50, consumptionFor100Km: 5},
-      {distance: 150, consumptionFor100Km: 6},
-      {distance: 250, consumptionFor100Km: 7},
-      {distance: 350, consumptionFor100Km: 8},
-      {distance: 450, consumptionFor100Km: 9}
-    ]
+  constructor(private cfpcs : CarbonFootprintComputeService) {
+    this.travels = this.cfpcs.getTravels()
     this.calculateDistanceAndAverage();
   }
 
@@ -42,19 +39,15 @@ export class CarbonFootprintComponent {
   public addTravel() {
     const distance = Math.ceil(Math.random() * 1000)
     const consumption = Math.ceil(Math.random() * 20)
-    this.travels.push({distance: distance, consumptionFor100Km: consumption})
+    this.cfpcs.addTravel({distance: distance, consumptionFor100Km: consumption})
     this.calculateDistanceAndAverage()
   }
 
   public calculateDistanceAndAverage() {
-    let total = 0;
-    let average = 0;
-    for (const travel of this.travels) {
-      total += travel.distance;
-      average += travel.consumptionFor100Km;
-    }
-    this.distance = total;
-    this.consumptionFor100Km = average / this.travels.length;
+    let resume = this.cfpcs.getTravelResume()
+    this.distance = resume.totaleDistance;
+    this.consumptionFor100Km = resume.averageConsumption
+    this.co2 = resume.totalCo2
   }
 
 
---
+++ src/app/services/carbon-footprint-compute.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarbonFootprintComputeService {
+
+  public travels: Array<any>
+
+  constructor() {
+    this.travels = [
+      {distance: 50, consumptionFor100Km: 5, quantityCo2: 10},
+      {distance: 150, consumptionFor100Km: 6, quantityCo2: 2},
+      {distance: 250, consumptionFor100Km: 7, quantityCo2: 5},
+      {distance: 350, consumptionFor100Km: 2, quantityCo2: 8},
+      {distance: 450, consumptionFor100Km: 1, quantityCo2: 12}
+    ]
+  }
+
+  public getTravels(): any[] {
+    return this.travels;
+  }
+
+  public addTravel(travel: any) {
+    travel.quantityCo2 = Math.ceil(travel.distance * travel.consumptionFor100Km * 2.3 / 100)
+    this.travels.push(travel)
+  }
+
+  public getTravelResume() {
+    let totalDistance = 0;
+    let averageConsumption = 0;
+    let totalCo2 = 0;
+    for (const travel of this.travels) {
+      totalDistance += travel.distance;
+      //calcul moyen de la consommation non pas par nombre de voyages
+      //mais par rapport au nombre total de km
+      averageConsumption += travel.consumptionFor100Km * travel.distance;
+      totalCo2 += travel.quantityCo2
+    }
+    return {totaleDistance: totalDistance, averageConsumption: averageConsumption / totalDistance, totalCo2: totalCo2}
+  }
+}
